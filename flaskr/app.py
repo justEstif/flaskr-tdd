9b3aa5ed90964415487caaa322957d76bdc91d52
Estifanos <estifanosbeyene21@gmail.com>
2023-03-22 16:23:31 -0400

    feat: add search page

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from functools import wraps
 from flask import Flask, render_template, request, session, flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -34,6 +35,16 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<title {self.title}>'
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
@@ -81,18 +92,29 @@ def add_entry():
     return redirect(url_for("index"))
 
 
-@app.route('/delete/<post_id>', methods=['GET'])
+@app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
-    """Delete post from database"""
+    """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db.session.query(Post).filter_by(id=post_id).delete()
+        new_id = post_id
+        db.session.query(Post).filter_by(id=new_id).delete()
         db.session.commit()
         result = {'status': 1, 'message': "Post Deleted"}
-        flash("The entry was deleted")
+        flash('The entry was deleted.')
     except Exception as e:
         result = {'status': 0, 'message': repr(e)}
     return jsonify(result)
+
+
+@app.route('/search/', methods=['GET'])
+def search():
+    query = request.args.get("query")
+    entries = db.session.query(Post)
+    if query:
+        return render_template('search.html', entries=entries, query=query)
+    return render_template('search.html')
 
 
 if __name__ == "__main__":
